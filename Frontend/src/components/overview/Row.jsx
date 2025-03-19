ea667359af81039f72404def5e60d96fb23ea760
Amr Waheed <amrmohamed877@gmail.com>
2025-03-19 06:27:12 +0200

    ..

--- Frontend/src/components/overview/Row.jsx
+++ Frontend/src/components/overview/Row.jsx
@@ -1,44 +1,52 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 function Row({ data }) {
-  const { id: productId, image, organic, price, productName, quantity } = data;
-  const navigate = useNavigate();
-  return (
-    <figure className="card">
-      <div className="card__emoji">{image}</div>
+    const {
+        id: productId,
+        image,
+        organic,
+        price,
+        productName,
+        quantity,
+    } = data;
+    const navigate = useNavigate();
+    return (
+        <figure className="card">
+            <div className="card__emoji">{image}</div>
 
-      <div className="card__title-box">
-        <h2 className="card__title">{productName}</h2>
-      </div>
+            <div className="card__title-box">
+                <h2 className="card__title">{productName}</h2>
+            </div>
 
-      <div className="card__details">
-        <div className="card__detail-box">
-          <h6 className="card__detail card__detail--organic">
-            {organic ? "Organic" : ""}
-          </h6>
-        </div>
+            <div className="card__details">
+                <div className="card__detail-box">
+                    <h6 className="card__detail card__detail--organic">
+                        {organic ? 'Organic' : ''}
+                    </h6>
+                </div>
 
-        <div className="card__detail-box">
-          <h6 className="card__detail">{quantity}</h6>
-        </div>
+                <div className="card__detail-box">
+                    <h6 className="card__detail">{quantity}</h6>
+                </div>
 
-        <div className="card__detail-box">
-          <h6 className="card__detail card__detail--price">{price}</h6>
-        </div>
-      </div>
-
-      <button
-        onClick={() => {
-          navigate(`/product/${productId}`);
-        }}
-        className="card__link"
-      >
-        <span>
-          Detail <i className="emoji-right">👉</i>
-        </span>
-      </button>
-    </figure>
-  );
+                <div className="card__detail-box">
+                    <h6 className="card__detail card__detail--price">
+                        {price}
+                    </h6>
+                </div>
+            </div>
+            <button
+                onClick={() => {
+                    navigate(`/product/${productId}`);
+                }}
+                className="card__link"
+            >
+                <span>
+                    Detail <i className="emoji-right">👉</i>
+                </span>
+            </button>
+        </figure>
+    );
 }
 
 export default Row;
